38994b44f139a391f6112ca5fc3cab2f9de8df12
Jungbum Woo <woojungbum12@gmail.com>
2019-09-07 12:55:43 +0900

    Updates

---
+++ precache-manifest.8da89dfa784ce07d0caddefa02b6ce3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35858b5b6a293ad13c4a5d5ec3ef7b2f",
+    "url": "/review_react_food/index.html"
+  },
+  {
+    "revision": "0701b04494357be3fbde",
+    "url": "/review_react_food/static/css/main.34ac1968.chunk.css"
+  },
+  {
+    "revision": "b36bde951ef79c0fb2ac",
+    "url": "/review_react_food/static/js/2.564c4ea5.chunk.js"
+  },
+  {
+    "revision": "0701b04494357be3fbde",
+    "url": "/review_react_food/static/js/main.c4c83d53.chunk.js"
+  },
+  {
+    "revision": "2569b89d4d0b5cffad2b",
+    "url": "/review_react_food/static/js/runtime~main.907f8d58.js"
+  }
+]);
